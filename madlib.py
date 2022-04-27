95ed86eadd5634639f011b0c9f367a0c26dcd214
Amélie Henry <102693116+AmelieHenry@users.noreply.github.com>
2022-04-27 09:22:24 -0600

    Add files via upload

---
+++ madlib.py
@@ -0,0 +1,12 @@
+# madlib project 
+# string concatenation: how to put strings together
+
+verb1 = input("verb:")
+adjectif = input("adjectif:")
+animal = input("animal:")
+verb2 = input("verb:")
+verb3 = input("verb:")
+noun3 = input("noun:")
+madlib = f'I love Leigh so much I could {verb1}. He is as {adjectif} as a {animal}. It makes me want to {verb2}. \
+Never stop learning to {verb3} so that you can find your {noun3}.'
+print(madlib)
